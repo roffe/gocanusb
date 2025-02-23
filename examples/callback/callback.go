beeb8f3207e0abd3995fd03bed204a4cf17ed0de
Roffe <joakim@roffe.nu>
2025-02-23 22:42:48 +0100

    update callback example

--- examples/callback/callback.go
+++ examples/callback/callback.go
@@ -52,16 +52,6 @@ func main() {
 }
 
 func callbackHandler(msg *gocanusb.CANMsg) uintptr {
-	// Beware of the callback, it's running in the C world and will replace the content of msg
-	// with the next message. If you want to keep the message to use later you need to copy it.
-	// For example if you send the message in a channel or similar.
-	msgCopy := &gocanusb.CANMsg{
-		Id:        msg.Id,
-		Timestamp: msg.Timestamp,
-		Flags:     msg.Flags,
-		Len:       msg.Len,
-	}
-	copy(msgCopy.Data[:], msg.Data[:])
-	log.Println("Callback: ", msgCopy.String())
+	log.Println("Callback: ", msg.String())
 	return 0
 }
